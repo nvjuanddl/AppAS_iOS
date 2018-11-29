d20de1c015ed3ecbe45028176cfef1a42677e5e1
Juan Delgado <juan.delgado@farmatodo.com>
2018-11-29 10:42:43 -0500

    Initial Commit

---
+++ AppAS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppAS
+//
+//  Created by Juan Delgado on 29/11/18.
+//  Copyright © 2018 Juan Delgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
